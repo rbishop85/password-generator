770881e033628d29b6e864b7a438edc6dc98395f
Robert Bishop <rbishop85@gmail.com>
2022-06-06 22:02:43 -0700

    Added class from office hours, started creating variable arrays for password characters

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function generatePassword(){
+ 
+  // Prompt user for password length and store in variable
+    // Validate Password length and make sure it's a number between 8 and 128
+  // Prompt user for need for special characters and store in boolean variable
+  // 
+
+// numbers
+var numbers = "0123456789".split("");
+console.log(numbers);
+// lowercase letters
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+console.log(lowerCase);
+// uppercase letters
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+console.log(upperCase);
+  // splits the individual characters into standalone variables in the array
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+//
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
