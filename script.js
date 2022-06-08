7930b2c5b5035357c67229a74044b917821fcfbd
Robert Bishop <rbishop85@gmail.com>
2022-06-07 19:38:24 -0700

    Created inputs for each of the pieces of data needed from user.  No checks on contents yet.

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 function generatePassword(){
- 
-  // Prompt user for password length and store in variable
-    // Validate Password length and make sure it's a number between 8 and 128
-  // Prompt user for need for special characters and store in boolean variable
-  // 
 
-// numbers
+    // Validate Password length and make sure it's a number between 8 and 128
+
+// Create variable for each type of password character and then splits them into separate array values.
 var numbers = "0123456789".split("");
-console.log(numbers);
-// lowercase letters
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-console.log(lowerCase);
-// uppercase letters
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-console.log(upperCase);
-// special characters
 var specChars = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~".split("");
+
+// Console Logs to verify
+console.log(numbers);
+console.log(lowerCase);
+console.log(upperCase);
 console.log(specChars);
+
+// Prompts for user input on Password requirements and stores response in variable.
+var passLength = prompt("Please enter required password length. (From 8 to 128) ");
+var lower = confirm("Does your password require lower case letters?");
+var upper = confirm("Does your password require upper case letters?");
+var numb = confirm("Does your password require numbers?");
+var spec = confirm("Does your password require special characters?");
+
+
+// Console logs to verify entries
+console.log("Password length is " + passLength + " characters.");
+console.log("Lower case letters required: " + lower + ".");
+console.log("Upper case letters required: " + upper + ".");
+console.log("Numbers required: " + lower + ".");
+console.log("Special characters required: " + lower + ".");
 
 
   // splits the individual characters into standalone variables in the array
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
 
-//
+
 }
 
 
