a17082c7988ab0bec28f8a9de58b42916031a1da
Robert Bishop <rbishop85@gmail.com>
2022-06-09 23:05:13 -0700

    Created a check to make sure final password met all criteria requested, next plan is to move final password creation into a function so if it fails to meet all criteria it will simply rerun until it does

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function generatePassword(){
 
 // Create variable for each type of password character and then splits them into separate array values.
 
-var numbers = "0123456789".split("");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "0123456789".split("");
 var specChars = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~".split("");
 
 // Function that asks user for password length, then notifies if the length isn't correct or isn't in numbers.
@@ -40,51 +40,92 @@ function charOptions(){
 
 var characters = charOptions();
 
-//Console log of resulting array
-console.log(characters);
-
 // Console log of resulting individual character choices
 console.log("Lower case characters: " + characters[0]);
 console.log("Upper case letters: " + characters[1]);
 console.log("Numbers: " + characters[2]);
 console.log("Special Characters: " + characters[3]);
 
-// Create a full list of potential characters based on criteria provided by user
-var charList = [];
+// 
+var charArray = [];
 
 if (characters[0]){
-  charList = charList.concat(lowerCase);
+  charArray = charArray.concat(lowerCase);
 }
 
 if (characters[1]){
-  charList = charList.concat(upperCase);
+  charArray = charArray.concat(upperCase);
 }
 
 if (characters[2]){
-  charList = charList.concat(numbers);
+  charArray = charArray.concat(numbers);
 }
 
 if (characters[3]){
-  charList = charList.concat(specChars);
+  charArray = charArray.concat(specChars);
 }
 
-console.log("Full list of character options for this password: " + charList);
+console.log("Full list of character options for this password: " + charArray);
 
 // Generate password based on randomly pulling number of characters requested by user and compiling them together.
 var newPassword = "";
 
-// for( var i = 0; i < length; i++ ) {
-//   newPassword = newPassword.concat(Math.floor(Math.random() * charList.length));
-// }
-
 for( var i = 0; i < length; i++ ) {
-  var choice = Math.floor(Math.random() * charList.length);
-  newPassword = newPassword.concat(charList[choice]);
+  var choice = Math.floor(Math.random() * charArray.length);
+  newPassword = newPassword.concat(charArray[choice]);
 }
 
-console.log("New Password: " + newPassword);
+console.log(newPassword);
 
-// var computer = Math.floor(Math.random() * choices.length);
+
+// Testing password validation
+
+var containL = lowerCase.some(element => {
+  if (newPassword.includes(element)) {
+    return true;
+  }
+  return false;
+});
+
+console.log("Password includes lower case: " + containL);
+
+var containU = upperCase.some(element => {
+  if (newPassword.includes(element)) {
+    return true;
+  }
+  return false;
+});
+
+console.log("Password includes upper case: " + containU);
+
+var containN = numbers.some(element => {
+  if (newPassword.includes(element)) {
+    return true;
+  }
+  return false;
+});
+
+console.log("Password includes numbers: " + containN);
+
+var containS = specChars.some(element => {
+  if (newPassword.includes(element)) {
+    return true;
+  }
+  return false;
+});
+
+console.log("Password includes special characters: " + containS);
+
+
+var passwordCheck = (containL === characters[0] && containU === characters[1] && containN === characters[2] && containS === characters[3]);
+
+console.log(passwordCheck);
+
+
+
+//
+
+
 
 
 return newPassword;
