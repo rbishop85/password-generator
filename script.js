92a4d6b899f13dae9dfe189dcfb781bddeeb0867
Robert Bishop <rbishop85@gmail.com>
2022-06-09 21:10:00 -0700

    Created function to ask user which characters they want in their password, then return those values in an array that is then split back into individual values outside the function

--- script.js
+++ script.js
@@ -7,21 +7,55 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var specChars = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~".split("");
 
-// Prompts for user input on Password requirements, checks against length criteria and stores response in variable.
-
+// Function that asks user for password length, then notifies if the length isn't correct or isn't in numbers.
+// Finally the excepted value is returned and set as the length variable.
 function passLength(){
   var length = prompt("Please enter required password length. (From 8 to 128)");
   if (length < 8 || length > 128 || isNaN(length)){
     alert("Please select a length between 8 and 128");
     return passLength();
-  }   else {
+  }  else {
       return length;
      }
 }
 
-length = passLength();
+var length = passLength();
 
+// Console log of length value retrieved from the function
 console.log("Password length is " + length + " characters.");
+
+// Function to ask user what types of characters they want, forcing them to try again if they select none, then spit out all results as an array
+function charOptions(){
+  var lower = confirm("Does your password require lower case letters?");
+  var upper = confirm("Does your password require upper case letters?");
+  var numb = confirm("Does your password require numbers?");
+  var spec = confirm("Does your password require special characters?");
+  if (!lower && !upper && !numb && !spec){
+    alert("You must select at least 1 type of character for your password.  Please try again.")
+    return charOptions();
+  }  else {
+    return [lower, upper, numb, spec];
+  }
+}
+
+var characters = charOptions();
+
+//Console log of resulting array
+console.log(characters);
+
+// Splitting resulting array back into individual values
+var lower = characters[0];
+var upper = characters[1];
+var numb = characters[2];
+var spec = characters[3];
+
+// Console log of resulting individual character choices
+console.log("Lower case characters: " + lower);
+console.log("Upper case letters: " + upper);
+console.log("Numbers: " + numb);
+console.log("Special Characters: " + spec);
+
+
 
 // var lower = confirm("Does your password require lower case letters?");
 // var upper = confirm("Does your password require upper case letters?");
